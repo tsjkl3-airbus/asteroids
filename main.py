5cc20851e4026e8ccab8c28b00f3fe4a3a73b6c7
tsjkl3-airbus <trey.saunders98@gmail.com>
2026-04-27 15:54:49 -0500

    A: Initial commit: add project files

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def main():
+    print("Starting Asteroids with pygame version:", pygame.version.ver)
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
